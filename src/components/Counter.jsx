98e5bcbcb4a48b48cab84296e368fefbabb63274
Sabrina Tjeng <sabriele@users.noreply.github.com>
2019-02-21 11:13:37 +0800

    Add buttons and provide functionality

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,8 +3,13 @@ import React, { Component } from 'react';
 export default class Counter extends Component {
   state = { id: 1, value: 0 };
 
-  handleClick = event => {
-    console.log(`Welcome has been ${event}ed!`);
+  handleIncrement = () => {
+    const value = this.state.value;
+    this.setState({ value: value + 1 });
+  };
+  handleDecrement = () => {
+    const value = this.state.value;
+    this.setState({ value: value - 1 });
   };
 
   render() {
@@ -12,6 +17,8 @@ export default class Counter extends Component {
     return (
       <React.Fragment>
         <div>{value}</div>
+        <button onClick={this.handleIncrement}>+</button>
+        <button onClick={this.handleDecrement}>-</button>
       </React.Fragment>
     );
   }
